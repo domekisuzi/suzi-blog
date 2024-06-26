95f534b797c03d5f7f556fa1c14c97641a940896
domekisuzi <domekisuzi@163.com>
2024-06-26 21:15:57 +0800

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,55 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
+const items = new Array(15).fill(null).map((_, index) => ({
+  key: index + 1,
+  label: `nav ${index + 1}`,
+}));
 
 function App() {
+
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Layout>
+        <Header style={{ display: 'flex', alignItems: 'center' }}>
+          <div className="demo-logo" />
+          <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              items={items}
+              style={{ flex: 1, minWidth: 0 }}
+          />
+        </Header>
+        <Content style={{ padding: '0 48px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+              style={{
+                background: colorBgContainer,
+                minHeight: 280,
+                padding: 24,
+                borderRadius: borderRadiusLG,
+              }}
+          >
+            Content
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
   );
 }
 
